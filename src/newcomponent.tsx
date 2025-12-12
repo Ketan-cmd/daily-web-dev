a7700fc8e581bc29c9ddab6fbd05901be2663303
Sruu <patilketankumar87@gmail.com>
2025-12-12 18:59:23 +0530

    Add new component

---
+++ src/newcomponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+const NewComponent = () => {
+    console.log("New Component Rendered");
+  return (
+    <div>NewComponent</div>
+  )
+}
+
+export default NewComponent
